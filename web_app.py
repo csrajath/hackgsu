fad87aa98f2a54fadbc67f36f53b9c2c899ef1d4
rajath <rajathcs.1996@gmail.com>
2020-11-22 08:52:43 +0530

    login logic

--- web_app.py
+++ web_app.py
@@ -8,6 +8,7 @@ Created on Sat Nov 21 15:25:08 2020
 
 from flask import Flask, request, render_template, send_from_directory
 import requests
+from api import mydb, seekers
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -16,10 +17,25 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 def homepage():
     return render_template('index.html')
 
-@app.route("/login")
-def loginpage():
-    return render_template('login.html')
+@app.route("/login/", methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    # users = mydb.db.users
+    # login_user = users.find_one({'name': request.form['username']})
+    # if login_user:
 
+@app.route("/upload/")
+def uploadresume():
+    return render_template('student_resume_upload.html')
+
+@app.route("/submit-profile/")
+def userprofile():
+    return render_template('student_profile_form.html')
+
+@app.route("/formsubmitted/")
+def usersubmission():
+    return render_template('student_submitted.html')
 
 if __name__ == "__main__":
     app.run(debug=False)
